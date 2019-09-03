23f5a0a02fa84961bff025abe5407e7cb5dbfb4b
JCamiloAgu <jcagudelo42@misena.edu.co>
2019-09-02 19:05:02 -0500

    Frist

---
+++ app/src/main/java/com/camilo/vuelingo/activities/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.camilo.vuelingo.activities
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.alejandrolora.mylibrary.ToolbarActivity
+import com.alejandrolora.seccion_07_drawer_recycler_card.toast
+import com.camilo.vuelingo.fragments.ArrivalsFragment
+import com.camilo.vuelingo.fragments.DeparturesFragment
+import com.camilo.vuelingo.fragments.HomeFragment
+import com.camilo.vuelingo.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : ToolbarActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbarToLoad(toolbar as Toolbar)
+
+        setNavDrawer()
+        setUserHeaderInformation()
+
+        if (savedInstanceState == null)
+            fragmentTransaction(HomeFragment())
+            navView.menu.getItem(0).isChecked = true
+    }
+
+    private fun setNavDrawer()
+    {
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, _toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun fragmentTransaction(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    private fun loadFragmentById(id: Int)
+    {
+        when (id)
+        {
+            R.id.nav_home -> fragmentTransaction(HomeFragment())
+            R.id.nav_departures -> fragmentTransaction(DeparturesFragment())
+            R.id.nav_arrivals -> fragmentTransaction(ArrivalsFragment())
+        }
+    }
+
+    private fun showMessageNavItemSelected(id: Int)
+    {
+        when (id)
+        {
+            R.id.nav_profile -> toast("Profile")
+            R.id.nav_settings -> toast("Settings")
+
+        }
+    }
+
+    private fun setUserHeaderInformation()
+    {
+        val name = navView.getHeaderView(0).findViewById<TextView>(R.id.txtName)
+        val email = navView.getHeaderView(0).findViewById<TextView>(R.id.txtEmail)
+
+        name?.let { name.text = getString(R.string.user_name) }
+        email?.let { email.text = getString(R.string.user_email) }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        showMessageNavItemSelected(item.itemId)
+        loadFragmentById(item.itemId)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
+    }
+}
---
+++ app/src/main/java/com/camilo/vuelingo/models/Flight.kt
@@ -0,0 +1,4 @@
+package com.camilo.vuelingo.models
+
+class Flight (val id: Int, val city: String, val imgResource: Int) {
+}
